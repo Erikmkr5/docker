930602dea09e4a05f0088f3ba28ab4c043d1dd26
Erikmkr5 <73306944+Erikmkr5@users.noreply.github.com>
2022-09-28 01:35:35 +0300

    Add files via upload

--- server/index.php
+++ server/index.php
@@ -1,22 +1,38 @@
 <html lang="en">
-<head>
-<title>Hello world page</title>
-    <link rel="stylesheet" href="style.css" type="text/css"/>
-</head>
 <body>
-<h1>Таблица пользователей данного продукта</h1>
-<table>
-    <tr><th>Id</th><th>Name</th><th>Surname</th></tr>
 <?php
-$mysqli = new mysqli("db", "user", "password", "appDB");
-$result = $mysqli->query("SELECT * FROM users");
-foreach ($result as $row){
-    echo "<tr><td>{$row['ID']}</td><td>{$row['name']}</td><td>{$row['surname']}</td></tr>";
-}
+	require_once ('mergesort.php');
+	$mas = array(5,7,2,3,9,4);
+	$mergesort = new Sort();
+	$sorted_mas=$mergesort->mergeSort($mas);
+	echo "<b/>sorted array: </b>";
+	foreach ($sorted_mas as $element){
+		echo $element . "  ";
+	}
+	
+	require_once ('commands.php');
+	$commands = new Commands();
+	echo "<br/><b/>ls: </b>";
+	echo $commands->ls() . "<br/><b/>ps: </b>";
+	echo $commands->ps() . "<br/><b/>whoami: </b>";
+	echo $commands->whoami() . "<br/><b/>id: </b>";
+	echo $commands->id() . "<br/>";
 ?>
-</table>
-<?php
-phpinfo();
-?>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=0" target="_blank">Ссылка на маленький зеленый круг </a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=1" target="_blank"> Ссылка на большой зеленый круг</a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=2" target="_blank"> Ссылка на маленький красный круг</a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=3" target="_blank"> Ссылка на большой красный круг</a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=4" target="_blank"> Ссылка на маленький зеленый прямоугольник</a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=5" target="_blank"> Ссылка на большой зеленый прямоугольник</a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=6" target="_blank"> Ссылка на маленький красный прямоугольник</a>
+</br>
+<a href ="http://localhost:8080/drawer.php?num=7" target="_blank"> Ссылка на большой красный прямоугольник</a>
 </body>
 </html>
